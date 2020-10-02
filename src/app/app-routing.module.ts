f0e390de322e9684c94714a25383a8a0aeb87535
Benjamin Weimer <bw91@gmx.de>
2020-10-02 11:28:19 +0200

    Use routing for tournaments and players

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TournamentListComponent} from './tournament-list/tournament-list.component';
+import {PlayerListComponent} from './player-list/player-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tournaments', component: TournamentListComponent },
+  { path: 'players', component: PlayerListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
